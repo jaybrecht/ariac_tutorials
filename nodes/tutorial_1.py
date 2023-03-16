39e1dc72054080aa26aa0c94b1bfe0d87d80babb
Justin Albrecht <jalbrecht23@gmail.com>
2023-03-16 16:31:18 -0400

    renamed node to tutorial_1

--- nodes/tutorial_1.py
+++ nodes/tutorial_1.py
@@ -5,11 +5,12 @@ Example script to start the competition.
 To test this script, run the following commands in separate terminals:
 
 - ros2 launch ariac_gazebo ariac.launch.py trial_name:=tutorial
-- ros2 run ariac_tutorials start_competition.py
+- ros2 run ariac_tutorials tutorial_1.py
 '''
 
 import rclpy
 from ariac_tutorials.competition_interface import CompetitionInterface
+
 
 def main(args=None):
     '''
@@ -23,6 +24,7 @@ def main(args=None):
     interface.start_competition()
     interface.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
